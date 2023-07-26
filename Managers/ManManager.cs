6fbb6693cf1cebb0eb26ee1049cf384b74fe6ddc
Pain Brioché <Kakoloukiam@gmail.com>
2023-07-26 22:44:08 +0200

    Partial fix to modded male bodies in 3.x

--- Managers/ManManager.cs
+++ Managers/ManManager.cs
@@ -1,4 +1,5 @@
 ﻿using HarmonyLib;
+using System;
 using static SceneEdit;
 
 namespace COM3D2.MaidLoader
@@ -15,21 +16,27 @@ namespace COM3D2.MaidLoader
         [HarmonyPostfix]
         private static void ManManagerConstructor_Postfix(PhotoManEditManager __instance)
         {
+            MPN body = (MPN)Enum.Parse(typeof(MPN), "body");
+
             foreach (string menuFile in GameUty.ModOnlysMenuFiles)
             {
                 //Only retains readable .menu containing mhead or mbody
                 if (menuFile.Contains("mhead") || menuFile.Contains("mbody"))
                 {
+                    PhotoManEditManager.menu_file_name_list_.Add(menuFile);
+
                     SMenuItem sMenuItem = new SMenuItem();
                     if (GetMenuItemSetUP(sMenuItem, menuFile, true))
                     {
                         //Add them to their corresponding list.
-                        if (sMenuItem.m_mpn == MPN.body)
+                        if (sMenuItem.m_mpn == body)
                         {
+                            MaidLoader.logger.LogDebug($"{menuFile} added to body");
                             __instance.man_body_menu_list.Add(sMenuItem);
                         }
-                        else if (sMenuItem.m_mpn == MPN.head)
+                        else
                         {
+                            MaidLoader.logger.LogDebug($"{menuFile} added to head");
                             __instance.man_head_menu_list.Add(sMenuItem);
                         }
                     }
